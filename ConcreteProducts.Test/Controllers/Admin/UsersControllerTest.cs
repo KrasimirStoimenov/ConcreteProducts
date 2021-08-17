274f3f0bf0d18999d49c1902d91c7ecaa214b7cf
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-08-17 21:32:28 +0300

    Tested UsersController

---
+++ ConcreteProducts.Test/Controllers/Admin/UsersControllerTest.cs
@@ -0,0 +1,41 @@
+﻿namespace ConcreteProducts.Test.Controllers.Admin
+{
+    using NUnit.Framework;
+    using MyTested.AspNetCore.Mvc;
+
+    using Microsoft.AspNetCore.Identity;
+
+    using UserController = Web.Areas.Admin.Controllers.UsersController;
+
+    public class UsersControllerTest
+    {
+        [Test]
+        public void PromoteShouldWorkAsExpected()
+            => MyController<UserController>
+                .Instance()
+                .WithUser()
+                .WithData(GetRole("Employee"), new IdentityUser { Id = "test1", UserName = "test", Email = "test@test.com" })
+                .Calling(c => c.Promote("test1"))
+                .ShouldReturn()
+                .RedirectToAction("All");
+        [Test]
+        public void DemoteShouldWorkAsExpected()
+            => MyController<UserController>
+                .Instance()
+                .WithUser()
+                .WithData(GetRole("Basic"), new IdentityUser { Id = "test1", UserName = "test", Email = "test@test.com" })
+                .Calling(c => c.Demote("test1"))
+                .ShouldReturn()
+                .RedirectToAction("All");
+
+
+        private static IdentityRole GetRole(string name)
+        {
+            return new IdentityRole
+            {
+                Name = name,
+                NormalizedName = name.ToUpper()
+            };
+        }
+    }
+}
