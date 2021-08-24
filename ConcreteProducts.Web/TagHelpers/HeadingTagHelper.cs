647ca24468344a6859d373de6274133d197e7402
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-08-24 15:56:32 +0300

    Add tag helper for headings

---
+++ ConcreteProducts.Web/TagHelpers/HeadingTagHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace ConcreteProducts.Web.TagHelpers
+{
+    using Microsoft.AspNetCore.Razor.TagHelpers;
+
+    [HtmlTargetElement("h2", Attributes = "heading-tag")]
+    public class HeadingTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.RemoveAll("heading-tag");
+            output.Attributes.Add("class", "headingMargin text-center offset-md-3 col-md-6 col-sm-12");
+        }
+    }
+}
