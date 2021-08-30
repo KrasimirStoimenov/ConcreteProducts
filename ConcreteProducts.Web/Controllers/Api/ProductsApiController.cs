f32da0801d679a7ffa7404e98fb1725607f1408e
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2021-08-30 15:00:29 +0300

    Introduce Web Api Controllers

---
+++ ConcreteProducts.Web/Controllers/Api/ProductsApiController.cs
@@ -0,0 +1,37 @@
+﻿namespace ConcreteProducts.Web.Controllers.Api
+{
+    using System.Threading.Tasks;
+    using System.Collections.Generic;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    using ConcreteProducts.Services.Products;
+    using ConcreteProducts.Services.Products.Models;
+
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsApiController : ControllerBase
+    {
+        private readonly IProductService productService;
+
+        public ProductsApiController(IProductService productService)
+            => this.productService = productService;
+
+        [HttpGet]
+        public async Task<IEnumerable<ProductListingServiceModel>> GetAllProducts()
+            => await this.productService.GetAllListingProductsAsync(searchTerm: null);
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductDetailsServiceModel>> GetProductDetails(int id)
+        {
+            var product = await this.productService.GetProductDetailsAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return product;
+        }
+    }
+}
---
+++ ConcreteProducts.Web/Controllers/Api/WarehouseProductsApiController.cs
@@ -0,0 +1,24 @@
+﻿namespace ConcreteProducts.Web.Controllers.Api
+{
+    using System.Threading.Tasks;
+    using System.Collections.Generic;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    using ConcreteProducts.Services.WarehouseProducts;
+    using ConcreteProducts.Services.WarehouseProducts.Models;
+
+    [Route("/api/warehouseProducts")]
+    [ApiController]
+    public class WarehouseProductsApiController
+    {
+        private readonly IWarehouseProductService warehouseProductService;
+
+        public WarehouseProductsApiController(IWarehouseProductService warehouseProductService)
+            => this.warehouseProductService = warehouseProductService;
+
+        [HttpGet]
+        public async Task<IEnumerable<WarehouseProductsServiceModel>> GetAllWarehouseProducts()
+            => await this.warehouseProductService.GetAllProductsInWarehouseAsync();
+    }
+}
